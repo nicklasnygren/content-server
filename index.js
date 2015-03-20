d3b27fcd03c6187542f2a91d5546ebcd351f37eb
Nicklas Nygren <hello@nickl.as>
2015-03-20 14:04:27 +0100

    Handle bulk card exports
    Squashed commit of the following:
    
    commit fce7c9eedda7690de99e0f6ea365e8d6cf155e43
    Author: Nicklas Nygren <hello@nickl.as>
    Date:   Fri Mar 20 14:00:55 2015 +0100
    
        Avoid async commit race
    
    commit 670e30f897a3d5171ca75d6d14a6f65e45c2549c
    Author: Nicklas Nygren <hello@nickl.as>
    Date:   Fri Mar 20 13:06:26 2015 +0100
    
        Simplify card dir creation with mkdirp
    
    commit 6550a11cee1ff0c860265b3b480e9789b67263d3
    Author: Nicklas Nygren <hello@nickl.as>
    Date:   Fri Mar 20 12:50:40 2015 +0100
    
        Use async queue for card blob commits
    
    commit 1e06c6ac14dd6315478da2f60188b45f1eca48e5
    Author: Nicklas Nygren <hello@nickl.as>
    Date:   Fri Mar 20 09:57:24 2015 +0100
    
        Handle org export and legacy cards
    
    commit 3256bb6b75b6d524e405659944ecae8933998f6c
    Author: Nicklas Nygren <hello@nickl.as>
    Date:   Fri Mar 20 09:50:19 2015 +0100
    
        hej
    
    commit 38bb7a289adafdd96eb58166a19e520c67a1cb11
    Author: Nicklas Nygren <hello@nickl.as>
    Date:   Fri Mar 20 09:28:26 2015 +0100
    
        Create CardBlobPath factory
    
    commit cffe58b47324d67470b995743457e994a7b2c721
    Author: Nicklas Nygren <hello@nickl.as>
    Date:   Fri Mar 20 09:22:57 2015 +0100
    
        Create CardBlob factory

--- index.js
+++ index.js
@@ -1,46 +1,32 @@
+'use strict';
+
 const DEFAULT_PORT = 5001;
 
+var async      = require('async');
 var debug      = require('debug')('main');
 var express    = require('express');
 var bodyParser = require('body-parser');
-var parseBlob  = require('./lib/parseBlob');
-var pathUtils  = require('./lib/pathUtils.js');
-var git        = require('./lib/git');
+var CardBlob   = require('./lib/factories/CardBlob');
 var app        = express();
-var server, latestOrgId;
+var server;
+
+var cardSaveQueue = async.queue(function (blob, callback) {
+  debug('Saving card: ' + blob.meta.id);
+
+  blob.saveAndCommit()
+  .then(function (msg) {
+    debug('Committed blob update with message "' + msg);
+    callback();
+  });
+}, 1);
 
 app.use(bodyParser.json());
-app.post('/', onPost)
-
-function onPost (req, res) {
-  res.send('Got payload');
-  var blob = parseBlob(req.body);
-  var path = pathUtils(blob);
-  var msg;
-
-  path.save()
-  .then(function (pathInfo) {
-    var method;
-
-    msg = getCommitMessage(pathInfo, blob);;
-    debug('Saved blob files to' + pathInfo.path);
-
-    if (latestOrgId === blob.meta.org) {
-      method = 'amend';
-      debug('Amending blob...');
-    }
-    else {
-      method = 'head';
-      debug('Committing blob...');
-    }
-
-    return git.commit[method](pathInfo.path, blob.meta.committer, msg);
-  })
-  .then(function () {
-    latestOrgId = blob.meta.org;
-    debug(`Committed blob diff with message "${msg}"`);
-  });
-};
+app.post('/card', function cardPost (req, res) {
+  var blob = CardBlob(req.body);
+  debug('Got request: ' + blob.meta.id);
+  cardSaveQueue.push(blob);
+  res.send('OK');
+});
 
 server = app.listen(DEFAULT_PORT, function () {
   var host, port;
@@ -51,15 +37,3 @@ server = app.listen(DEFAULT_PORT, function () {
   debug('Example app listening at http://localhost:%s', port);
 });
 
-function getCommitMessage (pathInfo, blob) {
-  var id = formatPath(blob.getParentString() + '.' + blob.meta.id);
-  var verb = pathInfo.createdNewDir ? 'create' : 'update';
-  return `${verb}(${id}): on org ${blob.meta.org}`;
-};
-
-function formatPath (rawPath) {
-  res = rawPath;
-  res = res.toLowerCase();
-  res = res.replace(/\s+/g, '_');
-  return res;
-};
